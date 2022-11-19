1618f6101bb9350883d3bc7c1f835085e3fb8bef
Sanket Kapse <sanket8397@gmail.com>
2022-11-19 13:43:53 -0700

    Additional functionality in main jframe.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Main extends JFrame {
 
     public Main(){
+        setTitle("Assignment 3 and 4");
         JMenuBar menuBar = new JMenuBar();
         FileMenu fileMenu = new FileMenu();
         HelpMenu helpMenu = new HelpMenu();
@@ -13,12 +14,15 @@ public class Main extends JFrame {
         StatusBar statusBar = new StatusBar();
         setLayout(new BorderLayout());
         add(statusBar, BorderLayout.SOUTH);
-
+        setResizable(false);
+        setExtendedState(MAXIMIZED_BOTH);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
         JFrame window = new Main();
-        window.setSize(500, 500);
+//        window.setSize(500, 500);
         window.setVisible(true);
+        window.setExtendedState(MAXIMIZED_BOTH);
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
