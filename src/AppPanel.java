a1f428adb5650b87f3dcab226312ede291861fe7
Sanket Kapse <sanket8397@gmail.com>
2022-11-19 16:05:42 -0700

    Changed App Panel with gridBagLayout.

--- src/AppPanel.java
+++ src/AppPanel.java
@@ -3,10 +3,19 @@ import java.awt.*;
 
 public class AppPanel extends JPanel {
     public AppPanel(){
-        setLayout(new GridLayout(1,2));
+//        setLayout(new GridLayout(1,2));
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
         CodePanel codePanel = new CodePanel();
         ClassPanel classPanel = new ClassPanel();
-        add(codePanel);
-        add(classPanel);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weightx = 0.3;
+        constraints.weighty = 1;
+        add(codePanel, constraints);
+        constraints.fill = GridBagConstraints.BOTH;
+        constraints.weighty = 1;
+        constraints.weightx = 0.7;
+        add(classPanel, constraints);
+
     }
 }
