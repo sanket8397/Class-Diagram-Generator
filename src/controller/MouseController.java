0cb322e6a507931e6e6971b5b9017290e4a92ad3
sohampatil <sohampatil.2303@gmail.com>
2022-12-02 04:41:16 -0700

    Segregated function and Added logic to not overlap dragged classes

--- src/controller/MouseController.java
+++ src/controller/MouseController.java
@@ -28,56 +28,33 @@ public class MouseController implements MouseListener, MouseMotionListener {
         statusBar.setStatus(message);
     }
 
-    int checkOverlapping(int x, int y) {
+    private boolean checkBoundaries(int x, int y) {
         if ((x + WIDTH / 2) > parentPanel.getSize().width || (x - WIDTH / 2) < 0 ||
                 (y + HEIGHT / 2) > parentPanel.getSize().height || (y - HEIGHT / 2) < 0)  {
             setStatus("Cannot create class because overlapping " +
                     "with screen boundaries");
-            return 0;
+            return true;
         }
+        return false;
+    }
+
+    private boolean checkCrossingClasses(int class_x, int class_y, int x, int y) {
+        return class_x <= x && class_x + WIDTH >= x && class_y <= y && class_y + HEIGHT >= y;
+    }
+
+    private boolean checkClassNameExists(String className) {
         ClassSource classSource = ClassSource.getInstance();
-
+        if (className.equals(""))
+            return true;
         for(ClassBox classBox: classSource.getClassBoxes()) {
-            int class_x = classBox.getRectangle().x;
-            int class_y = classBox.getRectangle().y;
-            if (class_x <= x && class_x + WIDTH >= x && class_y <= y && class_y + HEIGHT >= y) {
-                ClassBox toClass;
-                if (fromClass == null) {
-                    fromClass = classBox;
-                    return 1;
-                } else {
-                    toClass = classBox;
-                    String[] connectionOptions = {"Association", "Composition", "Inheritance"};
-                    String selection = (String) JOptionPane.showInputDialog(parentPanel,
-                            "Select model.Connection Type",
-                            "",
-                            JOptionPane.PLAIN_MESSAGE,
-                            null,
-                            connectionOptions, connectionOptions[0]);
-                    if (selection == null) {
-                        fromClass = null;
-                        return 2;
-                    }
-                    Connection connection;
-                    Line line = new Line();
-
-                    if (selection.equals(connectionOptions[0])) {
-                        connection = new Arrow(line);
-                    } else if (selection.equals(connectionOptions[1])) {
-                        connection = new Diamond(line);
-                    } else {
-                        connection = new Triangle(line);
-                    }
-
-                    connection.setFromClass(fromClass);
-                    connection.setToClass(toClass);
-                    classSource.addConnection(connection);
-                    fromClass = null;
-                    return 2;
-                }
+            if (classBox.getClassName().equals(className)) {
+                return true;
             }
         }
+        return false;
+    }
 
+    private boolean checkClassOverlapping(int x, int y, ClassSource classSource) {
         int check_top_left_x = x - WIDTH / 2, check_top_left_y = y - HEIGHT / 2;
         int check_bottom_right_x = x + WIDTH / 2, check_bottom_right_y = y + HEIGHT / 2;
         for (ClassBox classBox: classSource.getClassBoxes()) {
@@ -94,16 +71,67 @@ public class MouseController implements MouseListener, MouseMotionListener {
             if (left <= right && top <= bottom) {
                 setStatus("Cannot create class because overlapping " +
                         "with other class");
-                return 0;
+                return true;
             }
-
         }
+        return false;
+    }
+
+    private boolean drawConnection(ClassSource classSource, ClassBox classBox) {
+        String[] connectionOptions = {"Association", "Composition", "Inheritance"};
+        String selection = (String) JOptionPane.showInputDialog(parentPanel,
+                "Select Connection Type",
+                "",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                connectionOptions, connectionOptions[0]);
+        if (selection == null) {
+            fromClass = null;
+            return false;
+        }
+        Connection connection;
+        Line line = new Line();
+
+        if (selection.equals(connectionOptions[0])) {
+            connection = new Arrow(line);
+        } else if (selection.equals(connectionOptions[1])) {
+            connection = new Diamond(line);
+        } else {
+            connection = new Triangle(line);
+        }
+
+        connection.setFromClass(fromClass);
+        connection.setToClass(classBox);
+        classSource.addConnection(connection);
+        fromClass = null;
+        return true;
+    }
+
+    int checkBlockers(int x, int y) {
+        if (checkBoundaries(x, y))
+            return 0;
+        ClassSource classSource = ClassSource.getInstance();
+        for(ClassBox classBox: classSource.getClassBoxes()) {
+            int class_x = classBox.getRectangle().x;
+            int class_y = classBox.getRectangle().y;
+            if (checkCrossingClasses(class_x, class_y, x, y)) {
+                if (fromClass == null) {
+                    fromClass = classBox;
+                    return 1;
+                } else {
+                    if (drawConnection(classSource, classBox)) {
+                        return 2;
+                    }
+                }
+            }
+        }
+        if (checkClassOverlapping(x, y, classSource))
+            return 0;
         return 3;
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-
     }
 
     @Override
@@ -114,21 +142,25 @@ public class MouseController implements MouseListener, MouseMotionListener {
     @Override
     public void mouseClicked(MouseEvent e) {
         int box_x = e.getX(), box_y = e.getY();
-        int check = checkOverlapping(box_x, box_y);
-        if (check == 1) {
-            setStatus("Classes for from");
+        int check = checkBlockers(box_x, box_y);
+        if (check == 0) {
+            setStatus("Cannot overlap with other class' area and boundaries");
+        } else if (check == 1) {
+            setStatus("Class selected for connection");
         } else if (check == 2) {
-            setStatus("model.Line is drawn");
+            setStatus("Line is drawn");
         } else if (check == 3) {
             String className = JOptionPane.showInputDialog("Name");
-            if (className == null) {
-                setStatus("Classname cannot be Null");
+            if (className == null || checkClassNameExists(className)) {
+                setStatus("Classname cannot be Duplicate or null");
                 return;
             }
             ClassBox classBox = new ClassBox(className, box_x, box_y);
             ClassSource classSource = ClassSource.getInstance();
             classSource.addClassBox(classBox);
             setStatus("Class " + className + " is created");
+        } else {
+            setStatus("Something is wrong! For sure... ;)");
         }
     }
 
@@ -137,7 +169,7 @@ public class MouseController implements MouseListener, MouseMotionListener {
         for(ClassBox classBox: classSource.getClassBoxes()){
             int class_x = classBox.getRectangle().x;
             int class_y = classBox.getRectangle().y;
-            if (class_x <= x && class_x + WIDTH >= x && class_y <= y && class_y + HEIGHT >= y) {
+            if (checkCrossingClasses(class_x, class_y, x, y)) {
                 return classBox;
             }
         }
@@ -155,8 +187,12 @@ public class MouseController implements MouseListener, MouseMotionListener {
     @Override
     public void mouseReleased(MouseEvent e) {
         Rectangle rectangle = new Rectangle(e.getX(), e.getY(), WIDTH, HEIGHT);
-        if (dragClass != null && checkDragClass(e.getX(), e.getY()) != dragClass)
+        if (dragClass != null && checkDragClass(e.getX(), e.getY()) != dragClass && !checkBoundaries(e.getX() + WIDTH / 2, e.getY() + HEIGHT / 2) && !checkClassOverlapping(e.getX(), e.getY(), ClassSource.getInstance())) {
             dragClass.setRectangle(rectangle);
+            setStatus("Class dragged to other location");
+        } else {
+            setStatus("Not able to drag to that location");
+        }
         dragClass = null;
         ClassSource.getInstance().updatedByReference();
     }
