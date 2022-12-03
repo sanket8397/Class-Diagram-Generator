20a80fc8bcf4de6c52eff08fc0a65ddf9135157e
Rhishabh Hattarki <rhish9h@gmail.com>
2022-12-02 19:12:44 -0700

    javadocs done

--- src/view/GenerateDiagramButton.java
+++ src/view/GenerateDiagramButton.java
@@ -4,11 +4,13 @@ import controller.GenerateDiagramButtonController;
 
 import javax.swing.*;
 
+/**
+ * Button UI for generate diagram - clicking this will generate
+ * the diagrams in class panel based on the code written in code panel
+ */
 public class GenerateDiagramButton extends JButton {
     public GenerateDiagramButton() {
         setText("Generate Diagram");
-
-        System.out.println();
         addActionListener(new GenerateDiagramButtonController());
     }
 }
