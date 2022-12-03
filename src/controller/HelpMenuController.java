1ce9abea4d1cc5a2251f8f2fdb2c87f47af0e38c
Rhishabh Hattarki <rhish9h@gmail.com>
2022-12-02 19:38:08 -0700

    separated help menu controller and model

--- src/controller/HelpMenuController.java
+++ src/controller/HelpMenuController.java
@@ -1,55 +1,24 @@
 package controller;
 
+import model.HelpMenuData;
 import view.MessageDialog;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * This is a listener for the help menu to show project details
  * like developer list and design patterns
  */
 public class HelpMenuController implements ActionListener {
-    /**
-     * Show the list of developers working on the project
-     */
-    private void showDevelopers() {
-        List<String> developersList = new LinkedList<>();
-        developersList.add("Sanket Surendra Kapse");
-        developersList.add("Soham Prabhakar Patil");
-        developersList.add("Rhishabh Suhas Hattarki");
-        developersList.add("Anmol Girish More");
-        StringBuilder developers = new StringBuilder();
-        for (String developer : developersList)
-            developers.append(developer).append("\n");
-
-        new MessageDialog("Developers", developers.toString());
-    }
-
-    /**
-     * Show the design patterns used in the project
-     */
-    private void showDesignPatterns() {
-        List<String> designPatternsList = new LinkedList<>();
-        designPatternsList.add("Decorator");
-        designPatternsList.add("Singleton");
-        designPatternsList.add("Observer");
-        designPatternsList.add("Chain of Responsibility");
-        StringBuilder designPatterns = new StringBuilder();
-        for (String designPattern : designPatternsList)
-            designPatterns.append(designPattern).append("\n");
-
-        new MessageDialog("Design Patterns", designPatterns.toString());
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
+        HelpMenuData helpMenuData = new HelpMenuData();
+
         if (e.getActionCommand().equalsIgnoreCase("Developers")) {
-            showDevelopers();
+            new MessageDialog("Developers", helpMenuData.getDevelopers());
         } else if (e.getActionCommand().equalsIgnoreCase("Design Patterns")) {
-            showDesignPatterns();
+            new MessageDialog("Design Patterns", helpMenuData.getDesignPatterns());
         }
     }
 }
---
+++ src/model/HelpMenuData.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class HelpMenuData {
+    /**
+     * Get the list of developers working on the project
+     */
+    public String getDevelopers() {
+        List<String> developersList = new LinkedList<>();
+        developersList.add("Sanket Surendra Kapse");
+        developersList.add("Soham Prabhakar Patil");
+        developersList.add("Rhishabh Suhas Hattarki");
+        developersList.add("Anmol Girish More");
+        StringBuilder developers = new StringBuilder();
+        for (String developer : developersList)
+            developers.append(developer).append("\n");
+
+        return developers.toString();
+    }
+
+    /**
+     * Get the design patterns used in the project
+     */
+    public String getDesignPatterns() {
+        List<String> designPatternsList = new LinkedList<>();
+        designPatternsList.add("Decorator");
+        designPatternsList.add("Singleton");
+        designPatternsList.add("Observer");
+        designPatternsList.add("Chain of Responsibility");
+        StringBuilder designPatterns = new StringBuilder();
+        for (String designPattern : designPatternsList)
+            designPatterns.append(designPattern).append("\n");
+
+        return designPatterns.toString();
+    }
+}
