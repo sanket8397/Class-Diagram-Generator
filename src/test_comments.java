b49812fb1d9226f18d572849c165230a144ef4ef
Sanket Kapse <sanket8397@gmail.com>
2024-02-15 18:40:54 -0700

    Testing comments for 517
    Just for checking comments for 517 project

---
+++ src/test_comments.java
@@ -0,0 +1,14 @@
+//single line comments start
+/**
+  * multi lines comments start
+*/
+public class Main {
+
+    // indented multi line comment
+  public static void main(String[] args) {
+    /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+    System.out.println("Hello World");
+  }
+}
+// last line commented
